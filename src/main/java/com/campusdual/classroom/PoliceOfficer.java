7369bdf8961285d03c231632901f4ace6939c40b
Daniel <dani.v2509@outlook.com>
2024-10-17 13:21:19 +0200

    Solution

--- src/main/java/com/campusdual/classroom/PoliceOfficer.java
+++ src/main/java/com/campusdual/classroom/PoliceOfficer.java
@@ -8,4 +8,14 @@ public class PoliceOfficer extends Person {
 		super(name, surname);
 		this.squad = squad;
 	}
+
+	@Override
+	public void getDetails(){
+		StringBuilder msg = this.getNameBuilder();
+		msg.insert(0, "\nPOLICE OFFICER: ");
+		msg.append("\n\t Squad: ");
+		msg.append(this.squad);
+
+		System.out.println(msg.toString());
+	}
 }
